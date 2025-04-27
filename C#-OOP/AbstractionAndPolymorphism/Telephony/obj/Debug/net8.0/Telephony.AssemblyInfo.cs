e66d03e7a373f353531d020fb9dc6fe02836dafd
BorislavGerov <154229797+BorislavGerov@users.noreply.github.com>
2025-04-27 21:43:08 +0300

    .

--- C#-OOP/AbstractionAndPolymorphism/Telephony/obj/Debug/net8.0/Telephony.AssemblyInfo.cs
+++ C#-OOP/AbstractionAndPolymorphism/Telephony/obj/Debug/net8.0/Telephony.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Telephony")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9e365c5769b6178c29965d6baed07ec90ec7be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Telephony")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Telephony")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
